fc725ac28a6fc8dca5cfda1a5398913aae51f117
malaksalama827-bit <malaksalama827@gmail.com>
2025-12-10 17:12:12 +0200

    All Backend

---
+++ Backend/dermatologyclinicApp/dermatologyclinic/Controllers/MedicationsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dermatologyclinicApp.Models;
+
+namespace dermatologyclinicApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MedicationsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MedicationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Medication>>> GetMedications()
+        {
+            return await _context.Medications.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Medication>> GetMedication(int id)
+        {
+            var medication = await _context.Medications.FindAsync(id);
+            if (medication == null) return NotFound();
+            return medication;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Medication>> PostMedication(Medication medication)
+        {
+            _context.Medications.Add(medication);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetMedication", new { id = medication.Id }, medication);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMedication(int id, Medication medication)
+        {
+            if (id != medication.Id) return BadRequest();
+            _context.Entry(medication).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMedication(int id)
+        {
+            var medication = await _context.Medications.FindAsync(id);
+            if (medication == null) return NotFound();
+
+            _context.Medications.Remove(medication);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
